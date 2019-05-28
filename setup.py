eba5023ba160ffbbf0b31d12f6e8aa2bf719e2d3
yannick <yannickschroecker@gmail.com>
2019-05-28 12:12:28 -0400

    fixing setup.py (again)

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setuptools.setup(
     name="tabular",
     version="3.0",
     description="Tools for RL & IL experiments on tabular domains",
-    packages=setuptools.find_packages("tabular"),
+    packages=setuptools.find_packages(),
     python_requires=">3.6.0",
     install_requires=["numpy>=1.12", "matplotlib>=3.0.0", "scipy>=1.1"]
 )
